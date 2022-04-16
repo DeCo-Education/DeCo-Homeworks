87a5df25f2e1c6f64b2ab2b4fa82edf335f3ddce
ergonanaut-alpha <102749724+ergonanaut-alpha@users.noreply.github.com>
2022-04-16 06:50:22 -0700

    Ergoscript HW 2
    I will need to work on the implementation of Lambda. I understand the concept, but will need to work on actually implementation into code.

---
+++ ErgoScript-Developer-Course/batch-0/ergonaut_alpha/ErgoscriptHW2/ErgoscriptHW2.scala
@@ -0,0 +1,32 @@
+val bools: = True
+val wrong: = False
+
+if(true){
+    val num + 1
+} else {num - 1}
+
+{
+val Long: 10000L
+
+val Coll: Coll[Int] = Coll(2,4,6,8)
+
+val Pairs: (Int, Long) = (2, 10000000L)
+
+val MapPairs: Coll[(Int, Long)] = Coll((2,10000000L), (4, 10000000L), (6, 10000000L), (8,10000000L))
+}
+
+
+{
+    def Self: Box
+    
+    val OutputOne: Box = OUTPUTS(0)
+    val OutputTwo: Box = OUTPUTS(1)
+
+    val PinNumber: R4 = Coll[bytes]
+
+    val EqualOutputValue = OutputOne.value == OutputTwo.value
+    val SamePinUsed = R4 of Self == hashed r4 of OUTPUTS(0)
+
+    sigmaProp(EqualOutputValue) && SamePinUsed
+    
+}
